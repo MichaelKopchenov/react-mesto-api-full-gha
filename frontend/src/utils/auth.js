29dbc944bc19022a8d78ca0e94d021a52c5ac084
Mikhail Kopchenov <michaelkopchenov@yandex.ru>
2023-08-30 14:38:05 +0300

    ПР15, удалить ненужные символы

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -12,7 +12,6 @@ export const register = async (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ 
@@ -27,7 +26,6 @@ export const login = async (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ 
@@ -50,7 +48,6 @@ export const checkToken = async (token) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
-      'Accept': 'application/json',
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     }
